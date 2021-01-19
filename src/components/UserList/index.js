550bed9e86da68c946f0196078a400eecfc900c2
valmy <valmyericles@gmail.com>
2021-01-19 09:36:37 -0300

    user list ok

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import faker from 'faker';
 import { Container, Role, User, Avatar } from './styles';
 
-const UserRow = ({ nickname, isBot }) => {
+const UserRow = ({ nickname, isBot, imgUrl }) => {
   return (
     <User>
-      <Avatar/>
+      <Avatar className={isBot ? 'bot' : ''} src={imgUrl} />
 
       <strong>{nickname}</strong>
 
@@ -15,13 +16,26 @@ const UserRow = ({ nickname, isBot }) => {
 }
 
 function UserList() {
+  var loop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
   return (
     <Container>
       <Role>Disponível - 1</Role>
-      <UserRow nickname="Valmy Ericles"/>
+      <UserRow nickname="Valmy Ericles Nunes Machado" imgUrl={faker.random.image()}/>
 
       <Role>Offline - 18</Role>
-      <UserRow nickname="Alex_Machado" isBot/>
+
+      <UserRow nickname={faker.name.findName()} isBot />
+      
+      {loop.map(() => {
+        var randomName = faker.name.findName()
+        var img = faker.random.image()
+
+        return (
+          <UserRow nickname={randomName} imgUrl={img} />
+        )
+      })}
+
     </Container>
   )
 }
--- src/components/UserList/styles.js
+++ src/components/UserList/styles.js
@@ -27,14 +27,71 @@ export const Container = styled.div`
   }
 `;
 
-export const Role = styled.div`
+export const Role = styled.span`
+  margin-top: 20px;
 
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 500;
+  color: var(--gray);
 `;
 
 export const User = styled.div`
+  margin-top: 5px;
+  padding: 5px;
 
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+
+  border-radius: 4px;
+  background: transparent;
+  transition: background .2s;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.1);
+  }
+
+  > strong {
+    margin-left: 13px;
+    font-weight: 500;
+    color: var(--white);
+    opacity: 0.7;
+
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  > span {
+    margin-left: 9px;
+
+    background-color: var(--discord);
+    color: var(--white);
+    border-radius: 4px;
+    padding: 3px 5px;
+
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 11px;
+  }
 `;
 
 export const Avatar = styled.div`
+  flex-shrink: 0;
 
+  height: 32px;
+  width: 32px;
+  border-radius: 50%;
+
+  background-color: var(--primary);
+
+  background: url(${({ src }) => src });
+  background-position: center;
+  background-size: cover;
+
+  &.bot {
+    background-color: var(--mention-detail);
+  }
 `;
